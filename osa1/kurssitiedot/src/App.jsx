336f2c8e0d27514009fc5300dc836f0cc684ddae
Olli Haapasalo <olli.haapasalo@hotmail.com>
2024-02-16 13:14:39 +0200

    1.4

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,27 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course = {course} />
-      <Content part = {part1} />
-      <Content part = {part2} />
-      <Content part = {part3} />
-      <Total exercises = {[part1.exercises, part2.exercises, part3.exercises]} />
-
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -37,7 +35,10 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part ={props.part}/>
+      <Part part ={props.parts[0]}/>
+      <Part part ={props.parts[1]}/>
+      <Part part ={props.parts[2]}/>
+
 
     </div>
   )
@@ -54,7 +55,7 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p> <b>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]} </b></p>
+      <p> <b>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </b></p>
     </div>
   )
 }
