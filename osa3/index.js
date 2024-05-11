e03158783c52bc32c10b2f8373d7259e80061ffb
Olli Haapasalo <olli.haapasalo@hotmail.com>
2024-05-12 00:13:20 +0300

    3.2

--- osa3/index.js
+++ osa3/index.js
@@ -24,13 +24,23 @@ let notes = [
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+//app.get('/', (request, response) => {
+//  response.send('<h1>Hello World!</h1>')
+//})
 
 app.get('/api/persons', (request, response) => {
   response.json(notes)
 })
+
+
+app.get('/api/info', (request, response) => {
+    let people =  notes.length
+    time = new Date()
+    //console.log("time")
+
+    let rawtext = `Phonebook has info for ${people} people. ${time}`
+    response.json(rawtext)
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
