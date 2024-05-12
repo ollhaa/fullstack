0ceb459d05d1d43e5502708dbd8ec3dc4be532d1
Olli Haapasalo <olli.haapasalo@hotmail.com>
2024-05-12 22:28:43 +0300

    3.9

--- osa3/index.js
+++ osa3/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
+
 const app = express()
 
 app.use(express.json())
 app.use(morgan('combined'))
+app.use(cors())
 
 let persons = [
   {
