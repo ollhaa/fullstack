315f92f368470e359f2f0185adf5f55e18ec618a
Olli Haapasalo <olli.haapasalo@hotmail.com>
2024-05-12 10:18:51 +0300

    3.5

--- osa3/index.js
+++ osa3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -45,6 +47,38 @@ app.delete('/api/persons/:id', (request, response) => {
   //console.log(persons)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const newId = Math.floor(Math.random() * 10000 +1)
+  return newId
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  //console.log(request)
+  //console.log(body)
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  } else if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  //console.log("tt")
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 
